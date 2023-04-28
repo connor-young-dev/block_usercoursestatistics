f70a49a510941b30771e7c4dc6eac0d74bfd9e31
Connor Young <connor-young-dev@gmail.com>
2023-04-28 23:46:59 +0100

    Add some initial markup.

--- lang/en/block_usercoursestatistics.php
+++ lang/en/block_usercoursestatistics.php
@@ -4,3 +4,13 @@ $string['privacy:metadata'] = 'The User course statistics block does not store a
 $string['User course statistics'] = 'User course statistics';
 $string['usercoursestatistics:addinstance'] = 'Add a new user course statistics block block';
 $string['usercoursestatistics:myaddinstance'] = 'Add a new user course statistics block to Dashboard';
+
+// My Course statistics.
+$string['enrolments'] = 'Course Enrolments';
+$string['completions'] = 'Course Completions';
+$string['inprogress'] = 'Course in Progress';
+$string['badges'] = 'Badges Awarded';
+$string['certificates'] = 'Certificates Awarded';
+$string['completedcourses'] = 'Completed Courses';
+$string['inprogresscourses'] = 'in Progress';
+$string['awarded'] = 'Awarded';
